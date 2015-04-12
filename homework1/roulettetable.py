b56c6a68806aa28f86a0655651e076fd7a7696ef
Clemens Raffler <root676@users.noreply.github.com>
2015-04-12 13:16:48 +0200

    Update roulettetable.py

--- homework1/roulettetable.py
+++ homework1/roulettetable.py
@@ -2,10 +2,10 @@
 # Name:        RouletteTable
 # Purpose:     This class allows a player (from class Player) to play Roulette
 #
-# Author:      Dan
+# Author:      Stefan Fnord, Clemens Raffler, Daniel Zamojski
 #
 # Created:     25.03.2015
-# Copyright:   (c) Dan 2015
+# Copyright:   (c) Stefan Fnord, Clemens Raffler, Daniel Zamojski 2015
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python

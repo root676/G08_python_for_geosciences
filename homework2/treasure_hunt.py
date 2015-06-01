ed76a5b6d7a94926c2b2e9b9664b5b1e1a40bd3c
Coldrunner <danundmichael@hotmail.com>
2015-06-02 00:32:02 +0200

    added new code; and a sample at the end

--- homework2/treasure_hunt.py
+++ homework2/treasure_hunt.py
@@ -14,10 +14,14 @@
 import csv
 import urllib2
 import netCDF4
-
+#from Scientific.IO.NetCDF import NetCDFFile
+import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
+
+import h5py
+
 
 download_page = "http://rs.geo.tuwien.ac.at/downloads/cpa/"
 
@@ -33,12 +37,15 @@ response = urllib2.urlopen(download_page+filename1+".csv")
 column1 = "candle"
 
 
+# read header from file
+header1 = response.readline()[:-1].split(',')
+
+
 # load header from file
-for idx,elem in enumerate(response.readline()[:-1].split(',')):
+for idx,elem in enumerate(header1):
     # if answer of last question is in the header, then query the data
     if elem==column1:
         break
-
 else:
     print column1+" not found in header of the file - check the last question"
     exit
@@ -74,7 +81,97 @@ filename2part2 = "foolish"
 # download file
 print download_page+str(filename2part1)+'_'+filename2part2+'.nc'
 
-nc = netCDF4.Dataset(download_page+str(filename2part1)+'_'+filename2part2+'.nc')
-print nc
+
+# STEFANS CODE - download and plot netcdf
+
+#nc = netCDF4.Dataset(download_page+str(filename2part1)+'_'+filename2part2+'.nc')
+#print nc
 #response = urllib2.urlopen(download_page+filename2part1+'_'+filename2part2+'.nc')
+
+
+
+
+
+capitalcity = "canberra"
+
+# What planet has the shortest year?
+filename3_part2 = "mercury"
+filename3 =capitalcity +'_'+filename3_part2+'.npz'
+
+
+
+#response = requests.get('C:\Users\Dan\Downloads\canberra_mercury.npz', stream=True)
+#response = urllib2.urlopen(filename3,'wb')
+
+# load data  TODO: download data; DANIEL: WORKING HERE
+data = np.load('C:\Users\Dan\Downloads\canberra_mercury.npz')
+
+# gives back all available meta data
+#data.files
+
+# I?ll be . . . ., Arnold Schwarzenegger
+variable3 = "back"
+
+# giving back the data of 'back'
+variable3 = data[variable3]
+
+# An . . . .. a day keeps the doctor away!
+column3 = "apple"
+
+# load header from file
+for idx,elem in enumerate(header1):
+    # if answer of last question is in the header, then query the data
+    if elem==column3:
+        break
+else:
+    print column1+" not found in header of the file - check the last question"
+    exit
+
+# calculate mean value of the third column of the first downloaded file
+row3 = clue1_array[:,idx].mean()
+
+# load value from variabl3 with the specific row information
+filename4 = variable3[row3]
+
+# if value can taken as integer, then it will be changed to integer
+if filename4==int(filename4):
+    filename4 = int(filename4)
+
+# download the binary file
+response = urllib2.urlopen(download_page+filename4+".bin")
+
+# DANIEL: WORKING HERE
+
+
+
+
+
+#SAMPLE CODE FROM WEB FOR WRITING HDF5 files
+
+# write netcdffile
+file = NetCDFFile('rectilinear.nc', 'w')
+write_rectilinear(file, nodal, 'data', x_coord, y_coord)
+file.close()
+
+
+
+longitude = 0
+
+ # Create a new file using the default properties.
+fid = h5py.h5f.create(FILE)
+
+# Create the dataspace.  No maximum size parameter needed.
+dims = (DIM0, DIM1)
+space_id = h5py.h5s.create_simple(dims)
+
+# Create the dataset creation property list.  Set the layout to compact.
+dcpl = h5py.h5p.create(h5py.h5p.DATASET_CREATE)
+dcpl.set_layout(h5py.h5d.COMPACT)
+
+# Create the datasets using the dataset creation property list.
+dset = h5py.h5d.create(fid, DATASET, h5py.h5t.STD_I32BE, space_id, dcpl)
+
+# Write the data to the dataset.
+dset.write(h5py.h5s.ALL, h5py.h5s.ALL, wdata)
+
 
